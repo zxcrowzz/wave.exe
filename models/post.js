388c7d1f19e91fd133f1a408a0ae829e292cc3ac
zxcrowzz <pantsbro4@gmail.com>
2024-11-18 04:01:48 -0800

    Update post.js

--- models/post.js
+++ models/post.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     text: { type: String, required: true },
-    imgSrc: { type: String, default: "" },
-    time: { type: Date, default: Date.now },
-  });
+    imgSrc: { type: String, default: "" }, // Optional image source
+    videoSrc: { type: String, default: "" }, // Optional video source
+    time: { type: Date, default: Date.now }, // Timestamp of the post
+});
 
 const Post = mongoose.model('Post', postSchema);
 module.exports = Post;
